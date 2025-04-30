715f4b10384dd2283e56dc21b8e617998545ed82
dkmak02 <dkmak02@gmail.com>
2025-04-30 14:16:52 +0200

    begining of intl

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
-
+import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
 };
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(nextConfig);
