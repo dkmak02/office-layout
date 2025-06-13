5a209e343e5863889d8a8a8e0082bb0d1cf11f17
dkmak02 <dkmak02@gmail.com>
2025-06-13 11:22:56 +0200

    fiexex config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,22 +1,23 @@
-const { defineConfig } = require('cypress')
+import { defineConfig } from 'cypress'
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    supportFile: 'cypress/support/e2e.js',
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-    setupNodeEvents(_on: Cypress.PluginEvents, _config: Cypress.PluginConfigOptions) {
+    setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    env: {
-      apiUrl: 'http://localhost:7180'
-    },
+    baseUrl: 'http://localhost:3000',
     viewportWidth: 1280,
     viewportHeight: 720,
     video: false,
     screenshotOnRunFailure: true,
+    defaultCommandTimeout: 10000,
     requestTimeout: 10000,
     responseTimeout: 10000,
-    defaultCommandTimeout: 10000
-  }
+  },
+  component: {
+    devServer: {
+      framework: 'next',
+      bundler: 'webpack',
+    },
+  },
 })
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,4 +1,17 @@
-/// <reference types="cypress" />
+// ***********************************************************
+// This example support/e2e.ts is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
 
 // Import commands.js using ES2015 syntax:
 import './commands'
@@ -6,15 +19,4 @@ import './commands'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-// Hide fetch/XHR requests from command log
-const app = window.top;
-if (app) {
-  app.document.addEventListener('DOMContentLoaded', () => {
-    const style = app.document.createElement('style');
-    style.innerHTML = '.command-name-request, .command-name-xhr { display: none }';
-    app.document.head.appendChild(style);
-  });
-}
-
-// Prevent TypeScript from reading file as legacy script
-export {}
+/// <reference types="cypress" /> 
